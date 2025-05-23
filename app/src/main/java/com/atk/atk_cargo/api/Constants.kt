ba8fcf7df1e1d4a7353e6221ad91360f95689d5a
Prot0nX <ggroup2009@gmail.com>
2025-05-23 22:20:02 +0330

    اصلاح آدرس دامنه به Test

--- app/src/main/java/com/atk/atk_cargo/api/Constants.kt
+++ app/src/main/java/com/atk/atk_cargo/api/Constants.kt
@@ -1,7 +1,7 @@
 package com.atk.atk_cargo.api
 
 object Constants {
-    const val BASE_URL = "https://atk-nk.site/Cargo/"
+    const val BASE_URL = "https://atk-nk.site/Cargo/Test/"
     const val API_KEY = "atk_nk_9290VV42-38XQ02DI-F2WY4L2K-EJA7V682"
 
     @JvmStatic
