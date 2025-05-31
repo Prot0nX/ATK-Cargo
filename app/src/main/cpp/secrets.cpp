f9443a7e417d00429107d6da48625138e278e9d3
Prot0nX <ggroup2009@gmail.com>
2025-05-31 17:30:30 +0330

    اصلاح آدرس سرور

--- app/src/main/cpp/secrets.cpp
+++ app/src/main/cpp/secrets.cpp
@@ -4,7 +4,7 @@
 extern "C" {
 JNIEXPORT jstring JNICALL
 Java_com_atk_atk_1cargo_api_Secrets_getBaseUrl(JNIEnv* env, jobject /* this */) {
-    const char* baseUrl = "https://atk-nk.click/Cargo/Test/";
+    const char* baseUrl = "https://atk-nk.click/Cargo/";
     return env->NewStringUTF(baseUrl);
 }
 }
