762170938c3319b81738872ebd9fd6acfdc0cc56
Prot0nX <ggroup2009@gmail.com>
2025-06-05 16:37:38 +0330

    تغییر دامنه به سرور جدید

--- app/src/main/java/com/atk/atk_cargo/api/Constants.kt
+++ app/src/main/java/com/atk/atk_cargo/api/Constants.kt
@@ -1,7 +1,7 @@
 package com.atk.atk_cargo.api
 
 object Constants {
-    const val BASE_URL = "https://atk-nk.click/Cargo/Beta/"
+    const val BASE_URL = "https://atk-nk.top/Cargo/"
     const val API_KEY = "atk_nk_9290VV42-38XQ02DI-F2WY4L2K-EJA7V682"
 
     @JvmStatic
