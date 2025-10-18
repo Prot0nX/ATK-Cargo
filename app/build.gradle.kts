4152ce9f23d31b5ebdc5b5f24479e3f72a36f27e
Prot0nX <ggroup2009@gmail.com>
2025-10-18 21:00:24 +0330

    تغییر نسخه برنامه به 3.0.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 28
         targetSdk = 33
         versionCode = 1
-        versionName = "3.0.17"
+        versionName = "3.0.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
